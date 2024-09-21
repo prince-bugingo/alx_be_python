2ced693f3bee29b2d3c7bdf5699ac8d8450a31d8
Prince BUGINGO <princebugingolearn@gmail.com>
2024-09-21 14:26:58 +0200

    changed the directoy

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+#Perform Operations
+Addition = number1 + number2
+Subtraction = number1 - number2
+Multiplication = number1 * number2
+
+#Print the result with proper message
+print(f"Addition of {number1} and {number2} is {Addition}" )
+print(f"Subtraction of {number1} and {number2} is {Subtraction}" )
+print(f"Multiplication of {number1} and {number2} is {Multiplication}" )
